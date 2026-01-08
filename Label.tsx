262aa4c57fcfc00a30d03de2325b455c15e8e6b5
Nicolau Abel <nicolauabel.consult@gmail.com>
2026-01-08 20:11:49 +0100

    Delete Label.tsx

--- Label.tsx
+++
@@ -1,2 +0,0 @@
-import React from 'react';
-export function Label({children}:{children:React.ReactNode}){return <label className='block text-xs font-medium text-neutral-400 mb-2'>{children}</label>}
