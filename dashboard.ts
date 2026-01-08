2804cb73b4aba6d4730e2c4103b4574b29082af6
Nicolau Abel <nicolauabel.consult@gmail.com>
2026-01-08 20:17:16 +0100

    Delete dashboard.ts

--- dashboard.ts
+++
@@ -1,6 +0,0 @@
-import { api } from "./client";
-import type { DashboardStats } from "../types";
-export async function getStats() {
-  const res = await api.get<DashboardStats>("/api/dashboard/stats");
-  return res.data;
-}
