58e22887aff8aa5c0bef4d5be70ea4ea7dafa137
Nicolau Abel <nicolauabel.consult@gmail.com>
2026-01-08 20:23:07 +0100

    Delete tokenStorage.ts

--- tokenStorage.ts
+++
@@ -1,6 +0,0 @@
-const KEY = "cir_token";
-export const tokenStorage = {
-  get: () => localStorage.getItem(KEY),
-  set: (t: string) => localStorage.setItem(KEY, t),
-  clear: () => localStorage.removeItem(KEY),
-};
