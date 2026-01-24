0ccff0295962852d066bfd6d2b2b88ad5ac355f2
Nicolau Abel <nicolauabel.consult@gmail.com>
2026-01-24 18:52:15 +0100

    Update routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,17 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./Login";
-import MainLayout from "./MainLayout";
 import { RequireAuth, RequirePerm } from "./RequireAuth";
+import MainLayout from "./MainLayout";
 
-import RiskList from "../features/risk/RiskList";
-import RiskDetail from "../features/risk/RiskDetail";
-import RiskCreate from "../features/risk/RiskCreate";
-import SourceList from "../features/sources/SourceList";
-import SourceUpload from "../features/sources/SourceUpload";
-import UserList from "../features/users/UserList";
-import UserCreate from "../features/users/UserCreate";
-import UserEdit from "../features/users/UserEdit";
-import AuditList from "../features/audit/AuditList";
+import RiskList from "./RiskList";
+import RiskCreate from "./RiskCreate";
+import RiskDetail from "./RiskDetail";
+import Sources from "./Sources";
+import Users from "./Users";
+import Audit from "./Audit";
 
 export default function AppRoutes() {
   return (
@@ -19,24 +16,14 @@ export default function AppRoutes() {
       <Route path="/login" element={<Login />} />
 
       <Route element={<RequireAuth><MainLayout /></RequireAuth>}>
+        <Route path="/" element={<RiskList />} />
         <Route path="/risks" element={<RiskList />} />
-        <Route path="/risks/new" element={<RiskCreate />} />
-        <Route path="/risks/:id" element={<RiskDetail />} />
+        <Route path="/risks/new" element={<RequirePerm perm="risk:create"><RiskCreate /></RequirePerm>} />
+        <Route path="/risks/:id" element={<RequirePerm perm="risk:read"><RiskDetail /></RequirePerm>} />
 
-        <Route path="/sources" element={
-          <RequirePerm perm="sources:read">
-            <>
-              <SourceUpload />
-              <SourceList />
-            </>
-          </RequirePerm>
-        } />
-
-        <Route path="/users" element={<RequirePerm perm="users:read"><UserList /></RequirePerm>} />
-        <Route path="/users/new" element={<RequirePerm perm="users:create"><UserCreate /></RequirePerm>} />
-        <Route path="/users/:id" element={<RequirePerm perm="users:update"><UserEdit /></RequirePerm>} />
-
-        <Route path="/audit" element={<RequirePerm perm="audit:read"><AuditList /></RequirePerm>} />
+        <Route path="/sources" element={<RequirePerm perm="sources:read"><Sources /></RequirePerm>} />
+        <Route path="/users" element={<RequirePerm perm="users:read"><Users /></RequirePerm>} />
+        <Route path="/audit" element={<RequirePerm perm="audit:read"><Audit /></RequirePerm>} />
       </Route>
     </Routes>
   );
