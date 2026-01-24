15654405fe5988ab140025d458076792bc1db950
Nicolau Abel <nicolauabel.consult@gmail.com>
2026-01-24 18:28:27 +0100

    Initialize main.tsx with React and routing setup

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./app/routes";
+import { AuthProvider } from "./app/auth/AuthContext";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <AuthProvider>
+      <AppRoutes />
+    </AuthProvider>
+  </BrowserRouter>
+);
