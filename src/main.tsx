e3c974ffc5f9d310038d0059ae2a8d67ffbc6802
Nicolau Abel <nicolauabel.consult@gmail.com>
2026-01-24 18:38:50 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import AppRoutes from "./app/routes";
+import AppRoutes from "./routes";
 import { AuthProvider } from "./app/auth/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
